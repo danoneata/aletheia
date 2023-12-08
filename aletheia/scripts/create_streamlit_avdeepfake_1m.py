587fb41599100dd15bb4cca92042a9a6f443f0dd
Adriana Stan <adriana.stan@com.utcluj.ro>
2023-12-08 13:18:51 +0000

    updated AVDeepfake streamlit

--- aletheia/scripts/create_streamlit_avdeepfake_1m.py
+++ aletheia/scripts/create_streamlit_avdeepfake_1m.py
@@ -1,67 +1,65 @@
-from pathlib import Path
-import pdb
-
 import streamlit as st
 import os
 import json
 import pandas as pd
 import numpy as np
+import random
 
-st.title("AV-Deepfake-1M")
-BASE_DIR = Path("/mnt/private-share/speechDatabases/av-deepfake-1m")
+st.set_page_config(layout="wide")
+st.title('AV-Deepfake-1M')
+
+datadir = "/mnt/private-share/speechDatabases/av-deepfake-1m/"
 
 
 @st.cache_data
 def load_data(nrows):
-    with open(BASE_DIR / "val_metadata.json") as f:
-        a = json.load(f)
-
+    with open(os.path.join(datadir,"val_metadata.json")) as f:
+        a = json.load(f) 
     meta = {}
     for k in range(len(a)):
-        meta[a[k]["file"]] = a[k]
-
-    avail = [k.strip() for k in open(BASE_DIR / "available_files.txt").readlines()]
-
+        meta[a[k]['file']] = a[k]
+            
+    avail = [k.strip() for k in open(os.path.join(datadir,'available_files.txt')).readlines()]
     data = {}
     for k in avail[:nrows]:
-        data[k] = meta[k]
-
+        data[k]  = meta[k]
+        
     return data, len(avail)
+
 
 
 def filter_data(meta, modification, operation, nsamples):
     data = {}
     c = 0
-    for k in meta:
-        if meta[k]["modify_type"] == modification:
-            c += 1
-            f = open(BASE_DIR / "val_metadata" / k.replace("mp4", "json"))
+    keys = list(meta.keys())
+    random.shuffle(keys)
+    
+    for k in keys:
+        if meta[k]['modify_type']== modification: 
+            f = open(os.path.join(datadir+"/val_metadata", k.replace('mp4', 'json')))
             b = json.load(f)
-            if modification == "real":
+            if modification =='real':
                 data[k] = {}
-                data[k]["fs"] = []
-                data[k]["info"] = []
+                data[k]['fs'] = []
+                data[k]['info'] = []
+                c+=1
             else:
-                for m in b["operations"]:
-                    if (operation == "any") or (m["operation"] == operation):
+                for m in  b['operations']:
+                     if (operation=='any') or (m['operation'] == operation):
                         data[k] = {}
-                        data[k]["fs"] = meta[k]["fake_segments"]
-                        data[k]["info"] = []
-                        for n in b["operations"]:
-                            data[k]["info"].append(
-                                [
-                                    n["start"],
-                                    n["end"],
-                                    n["old_word"],
-                                    n["new_word"],
-                                    n["operation"],
-                                ]
-                            )
+                        data[k]['fs'] = meta[k]['fake_segments']
+                        data[k]['info'] = []
+                        data[k]['tsc'] = ' '.join([x['word'] for x in b['transcripts']])
+                        for n in b['operations']:
+                            data[k]['info'].append([n['start'], n['end'], n['old_word'], n['new_word'], n['operation']])
+                        
+                        c+=1
                         break
-        if c == nsamples:
-            break
+                        
+        if c == nsamples:           
+            break           
     return data
-
+            
 
 #### LOAD DATA
 data, navail = load_data(1000)
@@ -69,53 +67,61 @@ data, navail = load_data(1000)
 #### FILTER DATA
 with st.sidebar:
     modification = st.selectbox(
-        "Modification", ("both_modified", "audio_modified", "visual_modified", "real")
-    )
-    operation = st.selectbox("Operation", ("any", "insert", "replace", "delete"))
-    nsamples = st.slider("Max samples", 10, 100)
+        '**:blue[Modification]**',
+        ('both_modified', 'audio_modified', 'visual_modified', 'real'))
+    operation = st.selectbox(
+        '**:orange[Operation]**',
+        ('any', 'insert', 'replace', 'delete'))  
 
+    st.divider()
+    nsamples = st.slider('Max samples to display', 10, 100)
+    ncols = st.slider("Number of columns for display", 3, 8)
+    
 dd = filter_data(data, modification, operation, nsamples)
 st.write("Available files:", navail)
-st.write(
-    f"Found ",
-    len(dd),
-    " files with the specified operation ",
-    {operation},
-    " from a max of ",
-    nsamples,
-    " samples with the specified modification ",
-    {modification},
-)
 
+
+st.markdown(f"**Selected modification: :blue["+modification+"]**")
+st.markdown(f"**Selected operation: :orange[{operation}]**")
+#st.write(f"Found ",len(dd), " files with the specified operation ",{operation}," from a max of ",nsamples," samples with the specified modification ",{modification})
+st.write("Found ", len(dd), " samples")
 
 ##### SHOW DATA
-for line in range(len(dd) // 2):
-    for i, col in enumerate(st.columns(2)):
-        choice = list(dd.keys())[line * 2 + i]
-        with open(os.path.join("val", choice), "rb") as v:
+#ncols = 5
+for line in range(len(dd)//ncols):
+    for i, col in enumerate(st.columns(ncols)):
+        choice = list(dd.keys())[line*ncols+i] 
+        with open(os.path.join(datadir+'/val', choice), 'rb') as v:
             video_bytes = v.read()
         col.video(video_bytes)
-        col.markdown("**\[:orange[File:] " + os.path.split(choice)[0] + "**\]")
-        col.markdown(":blue[Fake segments:] ")
-        for j, times in enumerate(dd[choice]["fs"]):
-            info = str(times[0]) + "-" + str(times[1]) + " | "
-            info += dd[choice]["info"][j][-1].upper().ljust(7) + " | "
-            info += ":red[" + str(dd[choice]["info"][j][-3]) + "] -> "
-            info += ":green[" + str(dd[choice]["info"][j][-2]) + "]"
-
+        col.markdown("**\[:orange[File:] "+os.path.split(choice)[0] + "**\]")
+        col.markdown("**:blue[Fake segments]**")
+        for j, times in enumerate(dd[choice]['fs']):
+            info = "- "+str(times[0])+"-"+str(times[1])+" | "
+            info += ":orange["+dd[choice]['info'][j][-1].upper()+"] | "
+            info += ":red["+str(dd[choice]['info'][j][-3]) +"] -> "
+            info += ":green["+str(dd[choice]['info'][j][-2]) +"]"
             col.markdown(info)
+        col.markdown(dd[choice]['tsc'])
+    st.divider()    
+    
+if len(dd)%ncols:
+    for i, col in enumerate(st.columns(ncols)):
 
-if len(dd) % 2:
-    col, col1 = st.columns(2)
-    choice = list(dd.keys())[-1]
-    with open(os.path.join("val", choice), "rb") as v:
-        video_bytes = v.read()
-    col.video(video_bytes)
-    col.markdown("**\[:orange[File:] " + os.path.split(choice)[0] + "**\]")
-    col.markdown(":blue[Fake segments:] ")
-    for j, times in enumerate(dd[choice]["fs"]):
-        info = str(times[0]) + "-" + str(times[1]) + " | "
-        info += dd[choice]["info"][j][-1].upper().ljust(7) + " | "
-        info += ":red[" + str(dd[choice]["info"][j][-3]) + "] -> "
-        info += ":green[" + str(dd[choice]["info"][j][-2]) + "]"
-        col.markdown(info)
+        choice = list(dd.keys())[-1-i] 
+        with open(os.path.join(datadir+'/val', choice), 'rb') as v:
+            video_bytes = v.read()
+        col.video(video_bytes)
+        col.markdown("**\[:orange[File:] "+os.path.split(choice)[0] + "**\]")
+        col.markdown("**:blue[Fake segments]**")
+        for j, times in enumerate(dd[choice]['fs']):
+            info = "- "+str(times[0])+"-"+str(times[1])+" | "
+            info += ":orange["+dd[choice]['info'][j][-1].upper()+"] | "
+            info += ":red["+str(dd[choice]['info'][j][-3]) +"] -> "
+            info += ":green["+str(dd[choice]['info'][j][-2]) +"]"
+            col.markdown(info)
+        col.markdown(dd[choice]['tsc'])
+        
+        if i+1 == len(dd)%ncols:
+            break
+        
